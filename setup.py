6bc4c0af0481a6ba72c79da18c604add89056c2a
Dan Elkouby <streetwalrus@codewalr.us>
2016-04-08 18:00:51 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Shogun",
+    version = "0.1",
+    packages = find_packages(),
+    package_data = { "": ["templates/*"] },
+    install_requires = list(open("requirements.txt")),
+    author = """Dan "Streetwalrus" Elkouby""",
+    author_email = "streetwalrus@codewalr.us",
+    description = "A dead simple build system based on Ninja",
+    keywords = "ninja build c c++",
+    license = "MIT",
+    url = "https://github.com/Streetwalrus/shogun"
+)
